5c21902627f8ab1940a2483265e9df5da63b3f11
ROK-LAPTOP\syndr <syndrome5044@naver.com>
2017-07-09 22:22:11 +0900

    카메라 모션 스크립트 수정 / wsad로 카메라 전환 구현 / 임시 화물 데이터 삽입

--- isLab_RoRoShip/Assets/Scripts/CameraScript.cs
+++ isLab_RoRoShip/Assets/Scripts/CameraScript.cs
@@ -1,42 +1,68 @@
-﻿using System.Collections;
+﻿/*
+ * Created by Hyeongrok Heo
+ * 2017-07-09
+ * CameraScript.cs
+ * 카메라의 시점 전환 및 좌표 변환을 담당하는 스크립트
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
+	public float cameraSpeedX;
+	public float cameraSpeedY;
+
+	float cameraX, cameraY, cameraZ, angle;
 
 	// Use this for initialization
 	void Start () {
+		angle = 45;
+		//cameraX = 60.0f;
+		cameraY = 30.0f;
+		//cameraZ = -60.0f;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		cameraX = 85.0f * Mathf.Sin(angle * Mathf.Deg2Rad);
+		cameraZ = 85.0f * Mathf.Cos(angle * Mathf.Deg2Rad);
+
+		transform.position = new Vector3(cameraX, cameraY, cameraZ);
+		transform.rotation = Quaternion.LookRotation(new Vector3(-1*cameraX, -1*cameraY+5.0f, -1*cameraZ));
 		//transform.rotation = Quaternion.LookRotation(new Vector3(30.0f, 0.0f, 30.0f));
-		if (Input.GetKey(KeyCode.Alpha1)) {
-			transform.position = new Vector3(0.0f, 50.0f, 0.0f);
+		/*if (Input.GetKey(KeyCode.Alpha1)) {
+			transform.position = new Vector3(60.0f, 30.0f, -60.0f);
 			//transform.rotation = Quaternion.LookRotation(new Vector3(35.0f, -35.0f, 0.0f));
-			transform.rotation = Quaternion.LookRotation(new Vector3(0.0f, -1.0f, 0.0f));
-		}
-		if (Input.GetKey(KeyCode.Alpha2)) {
+			transform.rotation = Quaternion.LookRotation(new Vector3(-60.0f, -30.0f, 60.0f));
+		}*/
+		/*if (Input.GetKey(KeyCode.Alpha2)) { // 2번 시점
             transform.position = new Vector3(60.0f, 30.0f, -60.0f);
 			//transform.rotation = Quaternion.LookRotation(new Vector3(35.0f, -35.0f, 0.0f));
 			transform.rotation = Quaternion.LookRotation(new Vector3(-60.0f, -30.0f, 60.0f));
 		}
-        if (Input.GetKey(KeyCode.Alpha3)) {
+        if (Input.GetKey(KeyCode.Alpha3)) { // 3번 시점
             transform.position = new Vector3(60.0f, 30.0f, 60.0f);
 			transform.rotation = Quaternion.LookRotation(new Vector3(-60.0f, -30.0f, -60.0f));
-		}
-        /*if (Input.GetKey(KeyCode.Alpha3)) {
-            transform.Translate(35.0f, -135.0f, 0.0f);
-        }/*
-        /*if (Input.GetKey(KeyCode.D)) {
-            transform.Translate(0.0f, 0.0f, 0.1f);
+		}*/
+        if (Input.GetKey(KeyCode.W)) { // 카메라 상 이동
+            cameraY += cameraSpeedY;
         }
+        if (Input.GetKey(KeyCode.S)) { // 카메라 하 이동
+            cameraY -= cameraSpeedY;
+        }
+		if (Input.GetKey(KeyCode.A)) { // 카메라 좌 이동
+			angle += cameraSpeedX;
+		}
+		if (Input.GetKey(KeyCode.D)) { // 카메라 우 이동
+			angle -= cameraSpeedX;
+		}/*
 		if(Input.GetKey(KeyCode.Q)) {
 			transform.position = new Vector3(50.0f,50.0f,30.0f);
 			
 			//-85 -40 -70
 
 		}*/
-    }
+	}
 }
