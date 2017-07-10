e3d33825cdfbcb4ce14a45814904ee0f9ab06fe9
ROK-LAPTOP\syndr <syndrome5044@naver.com>
2017-07-10 19:07:54 +0900

    cargo 생성 환경 구축

---
+++ isLab_RoRoShip/Assets/Scripts/CargoCreater.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by Hyeongrok Heo
+ * 2017-07-10
+ * CargoCreater.cs
+ * 화물 생성 코드
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoCreater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) { // 화물 생성
+			
+		}
+	}
+}
