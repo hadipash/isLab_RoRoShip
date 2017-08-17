61ffda456454345c88d9844b662704470c2248b1
Iris-PC\Iris <rbcksdls@naver.com>
2017-08-18 02:51:03 +0900

    update test 2DViewer

--- isLab_RoRoShip/2DViewer/testRoRo2DViewer.py
+++ isLab_RoRoShip/2DViewer/testRoRo2DViewer.py
@@ -1,54 +1,93 @@
 import matplotlib.pyplot as plt
+import time
 
-mList = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 2, 0],
-    [1, 0, 1],
-    [1, 1, 0],
-    [1, 2, 0],
-    [2, 0, 2],
-    [2, 1, 0],
-    [2, 2, 0],
-]
-plt.figure(1)
-for i in range(9):
-    if mList[i][2] == 0:
-        myFc = 'w'
-    elif mList[i][2] == 1:
-        myFc = 'r'
-    elif mList[i][2] == 2:
-        myFc = 'b'
+class Floor:
+    def __init__(self):
+        # entrance = [x, y, height, width]
+        self.entrance = []
 
-    rectangle = plt.Rectangle((mList[i][0], mList[i][1]), height = 1, width = 1, fc = myFc, ec = 'black')
-    plt.gca().add_patch(rectangle)
-plt.axis('scaled')
+        # pillar = [x, y, height, width]
+        self.pillar = []
 
-plt.figure(2)
-for i in range(9):
-    if mList[i][2] == 0:
-        myFc = 'white'
-    elif mList[i][2] == 1:
-        myFc = 'red'
-    elif mList[i][2] == 2:
-        myFc = 'black'
+        # car = [x, y, height, width, type]
+        self.car = []
 
-    rectangle = plt.Rectangle((mList[i][0], mList[i][1]), height = 1, width = 1, fc = myFc, ec = 'black')
-    plt.gca().add_patch(rectangle)
-plt.axis('scaled')
+    pass
 
-plt.figure(3)
-for i in range(9):
-    if mList[i][2] == 0:
-        myFc = 'w'
-    elif mList[i][2] == 1:
-        myFc = 'r'
-    elif mList[i][2] == 2:
-        myFc = 'green'
+floor1 = Floor()
+floor2 = Floor()
+floor3 = Floor()
 
-    rectangle = plt.Rectangle((mList[i][0], mList[i][1]), height = 1, width = 1, fc = myFc, ec = 'black')
-    plt.gca().add_patch(rectangle)
-plt.axis('scaled')
+floors = [floor1, floor2, floor3]
 
+floors[0].entrance.append([0, 0, 1, 1])
+floors[1].entrance.append([0, 0, 1, 1])
+floors[2].entrance.append([0, 0, 1, 1])
+
+floors[0].pillar.append([1, 1, 1, 1])
+floors[1].pillar.append([1, 1, 1, 1])
+floors[2].pillar.append([1, 1, 1, 1])
+
+floors[0].car.append([0, 1, 1, 1, 0])
+floors[0].car.append([0, 2, 1, 1, 1])
+floors[1].car.append([0, 1, 1, 1, 2])
+floors[1].car.append([0, 2, 1, 1, 1])
+floors[2].car.append([0, 1, 1, 1, 2])
+floors[2].car.append([0, 2, 1, 1, 0])
+
+#make empty board
+for i in range(3):
+    plt.figure(i + 1)
+    for j in range(3):
+        for k in range(3):
+            rectangle = plt.Rectangle((j, k), height=1, width=1, fc='white', ec='black')
+            plt.gca().add_patch(rectangle)
+        plt.axis('scaled')
+
+#insert entrance
+for i in range(3):
+    for j in range(len(floors[i].entrance)):
+        entranceX = floors[i].entrance[j][0]
+        entranceY = floors[i].entrance[j][1]
+        entranceHeight = floors[i].entrance[j][2]
+        entranceWidth = floors[i].entrance[j][3]
+
+        plt.figure(i + 1)
+        rectangle = plt.Rectangle((entranceX, entranceY), height = entranceHeight,
+                                  width = entranceWidth, fc = 'gray', ec = 'black')
+        plt.gca().add_patch(rectangle)
+
+#insert pillar
+for i in range(3):
+    for j in range(len(floors[i].pillar)):
+        pillarX = floors[i].pillar[j][0]
+        pillarY = floors[i].pillar[j][1]
+        pillarHeight = floors[i].pillar[j][2]
+        pillarWidth = floors[i].pillar[j][3]
+
+        plt.figure(i + 1)
+        rectangle = plt.Rectangle((pillarX, pillarY), height = pillarHeight,
+                                  width = pillarWidth, fc = 'black', ec = 'black')
+        plt.gca().add_patch(rectangle)
+
+#insert car
+for i in range(3):
+    for j in range(len(floors[i].car)):
+        carX = floors[i].car[j][0]
+        carY = floors[i].car[j][1]
+        carHeight = floors[i].car[j][2]
+        carWidth = floors[i].car[j][3]
+        carType = floors[i].car[j][4]
+        carColor = ''
+        if carType == 0:
+            carColor = 'red'
+        elif carType == 1:
+            carColor = 'green'
+        elif carType == 2:
+            carColor = 'blue'
+        plt.figure(i + 1)
+        rectangle = plt.Rectangle((carX, carY), height = carHeight,
+                                  width = carWidth, fc = carColor, ec = 'black')
+        plt.gca().add_patch(rectangle)
 plt.show()
 
