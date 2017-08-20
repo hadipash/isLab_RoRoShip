090e877ab60bda9c22145086d15e37f6d5568089
Iris-PC\Iris <rbcksdls@naver.com>
2017-08-20 20:51:47 +0900

    solve TODO

--- Viewer/testRoRo2DViewer.py
+++ Viewer/testRoRo2DViewer.py
@@ -6,24 +6,24 @@ def drawPlots(cars):
 
     for i in range(len(ic.floors)):
         plt.figure(i + 1)
-        rectangle = plt.Rectangle((0, 0), height=ic.floors[i].length, width=ic.floors[i].width, fc='white', ec='black')
+        rectangle = plt.Rectangle((0, -ic.floors[i].length), height=ic.floors[i].length, width=ic.floors[i].width, fc='white', ec='black')
         plt.gca().add_patch(rectangle)
         #plt.axis('scaled')
 
     # insert entrance
     for i in range(len(ic.floors)):
         plt.figure(i + 1)
-        ent = ic.floors[i].entrances
         for ent in ic.floors[i].entrances:
             entranceX = ent.coordinate.x
             entranceY = ent.coordinate.y
             entranceHeight = ent.length
             entranceWidth = ent.width
 
-            rectangle = plt.Rectangle((entranceX, entranceY), height=entranceHeight,
+            rectangle = plt.Rectangle((entranceX, -(entranceY + entranceHeight)), height=entranceHeight,
                                       width=entranceWidth, fc='gray', ec='black')
             plt.gca().add_patch(rectangle)
         plt.axis('scaled')
+
     # insert pillar
     for i in range(len(ic.floors)):
         plt.figure(i + 1)
@@ -33,15 +33,56 @@ def drawPlots(cars):
             pillarHeight = obs.length
             pillarWidth = obs.width
 
-            rectangle = plt.Rectangle((pillarX, pillarY), height=pillarHeight,
+            rectangle = plt.Rectangle((pillarX, -(pillarY + pillarHeight)), height=pillarHeight,
                                       width=pillarWidth, fc='black', ec='black')
             plt.gca().add_patch(rectangle)
         plt.axis('scaled')
     # TODO: add also ic.floors[i].notLoadable, ic.floors[i].ramps, ic.floors[i].slopes
     # ic.floors[i].decks ignore for this moment
 
-    # insert car
+    #insert notLoadble
+    for i in range(len(ic.floors)):
+        plt.figure(i + 1)
+        for nLoad in ic.floors[i].notLoadable:
+            nLoadX = nLoad.coordinate.x
+            nLoadY = nLoad.coordinate.y
+            nLoadHeight = nLoad.length
+            nLoadWidth = nLoad.width
 
+            rectangle = plt.Rectangle((nLoadX, -(nLoadY + nLoadHeight)), height=nLoadHeight,
+                                      width=nLoadWidth, fc='orange', ec='black')
+            plt.gca().add_patch(rectangle)
+        plt.axis('scaled')
+
+    # insert ramps
+    for i in range(len(ic.floors)):
+        plt.figure(i + 1)
+        for ramp in ic.floors[i].ramps:
+            rampX = ramp.coordinate.x
+            rampY = ramp.coordinate.y
+            rampHeight = ramp.length
+            rampWidth = ramp.width
+
+            rectangle = plt.Rectangle((rampX, -(rampY + rampHeight)), height=rampHeight,
+                                          width=rampWidth, fc='purple', ec='black')
+            plt.gca().add_patch(rectangle)
+        plt.axis('scaled')
+
+    # insert slopes
+    for i in range(len(ic.floors)):
+        plt.figure(i + 1)
+        for slope in ic.floors[i].slopes:
+            slopeX = slope.coordinate.x
+            slopeY = slope.coordinate.y
+            slopeHeight = slope.length
+            slopeWidth = slope.width
+
+            rectangle = plt.Rectangle((slopeX, -(slopeY + slopeHeight)), height=slopeHeight,
+                                          width=slopeWidth, fc='gray', ec='black')
+            plt.gca().add_patch(rectangle)
+        plt.axis('scaled')
+
+    # insert car
     for i in range(len(ic.floors)):
         plt.figure(i + 1)
         for car in cars:
@@ -65,11 +106,11 @@ def drawPlots(cars):
                 carColor = 'blue'
             elif carType == "Wheel_Loaders":
                 carColor = 'skyblue'
-                
+
             if carFloor == i:
-                rectangle = plt.Rectangle((carX, carY), height=carHeight,
+                rectangle = plt.Rectangle((carX, -(carY + carHeight)), height=carHeight,
                                       width=carWidth, fc=carColor, ec='black')
                 plt.gca().add_patch(rectangle)
 
-    plt.axis('scaled')
+        plt.axis('scaled')
     plt.show()
